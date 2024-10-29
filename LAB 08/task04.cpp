0af05385792da1c860a500d4569970414e153ece
muhamad-hammad <m.hammad.bhatti09@gmail.com>
2024-10-29 23:33:06 +0500

    task 04 logic corrected and task 5 completed

--- LAB 08/task04.cpp
+++ LAB 08/task04.cpp
@@ -6,8 +6,173 @@
         //    4   7
         //         \
         //          8   
-// You are tasked to take a value X from the user and ask whether to ceil or floor the value. If ceil
-// is selected find the closest value to X in our case 7 at level three right child because if we ceil 6
-// we get 7 (Yes, I am familiar with the logic of how ceil and floor works with floating points but
-// this scenario is different so in the case of ceil add 1 to X and in the case of floor subtract 1 from
-// x then find the value). Also find multiple occurrences if it as well.
+// You are tasked to take a value X from the user and ask
+// whether to ceil or floor the value. If ceil
+// is selected find the closest value to X in our case 7 at 
+// level three right child because if we ceil 6
+// we get 7 (Yes, I am familiar with the logic of how ceil 
+// and floor works with floating points but
+// this scenario is different so in the case of ceil add 1 to
+// X and in the case of floor subtract 1 from
+// x then find the value). Also find multiple occurrences if
+// it as well.
+
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+class Node{
+    public:
+        int key;
+        string name;
+        Node* leftChild;
+        Node* rightChild;
+        Node(int key, string name){
+            this->key = key;
+            this->name = name;
+            this->leftChild = NULL;
+            this->rightChild = NULL;
+        }
+        string toString(int key){
+            return "Key: " + to_string(key) + "\n";
+        }
+};
+
+class BinaryTree{
+    Node* root;
+    public:
+        BinaryTree(){
+            root = NULL;
+        }
+        void addNode(int key,string name){
+            Node* newNode = new Node(key,name);
+            if(root == NULL){
+                root = newNode;
+            }
+            else{
+                Node* current = root;
+                Node* parent;
+                while(true){
+                    parent = current;
+                    if(key < current->key){
+                        current = current->leftChild;
+                        if(current == NULL){
+                            parent->leftChild = newNode;
+                            return;
+                        }
+                    }
+                    else{
+                        current = current->rightChild;
+                        if(current == NULL){
+                            parent->rightChild = newNode;
+                            return;
+                        }
+                        }
+                        }
+                }
+
+        }
+        
+void ceil(Node* node, int key) {
+    if (node == NULL) return;
+    
+    Node* current = node;
+    Node* ceilNode = NULL;
+    
+    while (current != NULL) {
+        if (current->key == key + 1) {
+            ceilNode = current;
+            break;
+        }
+        
+        if (current->key > key) {
+            ceilNode = current;
+            current = current->leftChild; // Move left to find a closer ceil if exists
+        } else {
+            current = current->rightChild; // Move right if current node is less than or equal to key
+        }
+    }
+    
+    if (ceilNode != NULL) {
+        cout << "Ceil: " << ceilNode->key << endl;
+    } else {
+        cout << "No ceil found." << endl;
+    }
+}
+
+void floor(Node* node, int key) {
+    if (node == NULL) return;
+    
+    Node* current = node;
+    Node* floorNode = NULL;
+    
+    while (current != NULL) {
+        if (current->key == key - 1) {
+            floorNode = current;
+            break;
+        }
+        
+        if (current->key < key) {
+            floorNode = current;
+            current = current->rightChild; // Move right to find a closer floor if exists
+        } else {
+            current = current->leftChild; // Move left if current node is greater than or equal to key
+        }
+    }
+    
+    if (floorNode != NULL) {
+        cout << "Floor: " << floorNode->key << endl;
+    } else {
+        cout << "No floor found." << endl;
+    }
+}
+
+        void search(){
+                cout << "Enter the value to search: ";
+                int key;
+                cin >> key;
+                Node* current = root;   
+                while(current->key != key){
+                    if(key < current->key){
+                        current = current->leftChild;
+                    }
+                    else if(key > current->key){
+                        current = current->rightChild;
+                    } else{
+                        break;
+                    }
+                }
+                string choice;
+                while (choice != "ceil" && choice != "floor"){
+                cout << "put correct choice that you want to find the ceil or floor of the value? (ceil/floor): ";
+                cin >> choice;
+                if(choice == "ceil"){
+                    ceil(root, key);
+                    break;
+                }
+                else if(choice == "floor"){
+                    floor(root, key);
+                    break;
+                }
+                }
+        }
+
+
+};
+
+int main(){
+        BinaryTree tree;
+        tree.addNode(10,"A");
+        tree.addNode(15,"B");
+        tree.addNode(20,"C");
+        tree.addNode(25,"D");
+        tree.addNode(30,"E");
+        tree.addNode(35,"F");
+        tree.addNode(40,"G");
+        tree.addNode(45,"H");
+        tree.addNode(50,"I");
+        tree.addNode(55,"J");
+        
+        tree.search();
+}
--- LAB 08/task05.cpp
+++ LAB 08/task05.cpp
@@ -18,4 +18,155 @@
         //        /
         //       6
 
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Node {
+public:
+    int key;
+    string name;
+    Node* leftChild;
+    Node* rightChild;
+
+    Node(int key, string name) {
+        this->key = key;
+        this->name = name;
+        this->leftChild = NULL;
+        this->rightChild = NULL;
+    }
+
+    string toString(int key) {
+        return name + " has the key " + to_string(key) + " \n";
+    }
+};
+
+class BinaryTree {
+private:
+    Node* root;
+
+public:
+    BinaryTree() {
+        root = NULL;
+    }
+
+    void addNode(int key, string name) {
+        Node* newNode = new Node(key, name);
+        if (root == NULL) {
+            root = newNode;
+        } else {
+            Node* current = root;
+            Node* parent;
+            while (true) {
+                parent = current;
+                if (key < current->key) {
+                    current = current->leftChild;
+                    if (current == NULL) {
+                        parent->leftChild = newNode;
+                        return;
+                    }
+                } else {
+                    current = current->rightChild;
+                    if (current == NULL) {
+                        parent->rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    void printTree(Node* node) {
+        if (node != NULL) {
+            printTree(node->leftChild);
+            cout << node->toString(node->key);
+            printTree(node->rightChild);
+        }
+    }
+
+    void printTree() {
+        printTree(root);
+    }
+
+    void inOrerTraversal(Node* node, int* arr, int& index) {
+        if (node != NULL) {
+            inOrerTraversal(node->leftChild, arr, index);
+            arr[index++] = node->key;
+            inOrerTraversal(node->rightChild, arr, index);
+        }
+    }
+
+    void inOrerTraversal(int* arr, int& index) {
+        index = 0;
+        inOrerTraversal(root, arr, index);
+    }
+
+    Node* sortedArrayToBST(int* arr, int start, int end) {
+        if (start > end) return nullptr;
+        int mid = (start + end) / 2;
+        Node* node = new Node(arr[mid], "");
+        node->leftChild = sortedArrayToBST(arr, start, mid - 1);
+        node->rightChild = sortedArrayToBST(arr, mid + 1, end);
+        return node;
+    }
+
+    void buildFromSortedArray(int* arr, int n) {
+        root = sortedArrayToBST(arr, 0, n - 1);
+    }
+};
+
+int* arrMerge(int* arr1, int* arr2, int size1, int size2) {
+    int* arr = new int[size1 + size2];
+    int index = 0, i = 0, j = 0;
+    while (i < size1 && j < size2) {
+        if (arr1[i] < arr2[j]) {
+            arr[index++] = arr1[i++];
+        } else {
+            arr[index++] = arr2[j++];
+        }
+    }
+    while (i < size1) arr[index++] = arr1[i++];
+    while (j < size2) arr[index++] = arr2[j++];
+    return arr;
+}
+
+int main() {
+    BinaryTree tree1;
+    tree1.addNode(5, "Node 1");
+    tree1.addNode(3, "Node 2");
+    tree1.addNode(6, "Node 3");
+    tree1.addNode(2, "Node 4");
+    tree1.addNode(4, "Node 5");
+    tree1.printTree();
+    cout << endl;
+
+    BinaryTree tree2;
+    tree2.addNode(2, "Node 6");
+    tree2.addNode(1, "Node 7");
+    tree2.addNode(3, "Node 8");
+    tree2.addNode(7, "Node 9");
+    tree2.addNode(6, "Node 10");
+    tree2.printTree();
+    cout << endl;
+
+    int size1 = 5;
+    int size2 = 5;
+    int* arr1 = new int[size1];
+    int* arr2 = new int[size2];
+    int index1 = 0, index2 = 0;
+
+    tree1.inOrerTraversal(arr1, index1);
+    tree2.inOrerTraversal(arr2, index2);
+
+    int* mergedArr = arrMerge(arr1, arr2, index1, index2);
+
+    BinaryTree tree3;
+    tree3.buildFromSortedArray(mergedArr, index1 + index2);
+    tree3.printTree();
+
+    delete[] arr1;
+    delete[] arr2;
+    delete[] mergedArr;
+
+    return 0;
+}
